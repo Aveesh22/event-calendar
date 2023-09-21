f202827356412c46b120bc31c18042edaeb1d532
Aveesh22 <68564656+Aveesh22@users.noreply.github.com>
2023-09-20 22:54:41 -0400

    Add preliminary behavior to EventCalendar class through multiple methods

--- src/main/java/eventorganizer/EventCalendar.java
+++ src/main/java/eventorganizer/EventCalendar.java
@@ -8,12 +8,59 @@ public class EventCalendar
 {
     private Event [] events; //the array holding the list of events
     private int numEvents; //current number of events in the array
-    private int find(Event event) { } //search an event in the list
-    private void grow() { } //increase the capacity by 4
-    public boolean add(Event event) { }
-    public boolean remove(Event event) { }
-    public boolean contains(Event event) { }
-    public void print() { } //print the array as is
+
+    public EventCalendar()
+    {
+
+        numEvents = events.length;
+    }
+    private int find(Event event) //search an event in the list
+    {
+        int index = -1;
+        for(int i = 0; i < events.length; i++)
+        {
+            if(events[i].equals(event))
+            {
+                index = i;
+            }
+        }
+        return index;
+    }
+    private void grow() //increase the capacity by 4
+    {
+        Event[] temp = new Event[numEvents+4];
+        for(int i = 0; i < events.length; i++)
+        {
+            temp[i] = events[i];
+        }
+        events = temp;
+    }
+    public boolean add(Event event)
+    {
+        
+
+    }
+    public boolean remove(Event event)
+    {
+
+    }
+    public boolean contains(Event event)
+    {
+        for(int i = 0; i < events.length; i++)
+        {
+            if(events[i].equals(event))
+                return true;
+        }
+        return false;
+    }
+
+    public void print() //print the array as is
+    {
+        for(int i = 0; i < events.length; i++)
+        {
+            System.out.println(events[i]);
+        }
+    }
     public void printByDate() { } //ordered by date and timeslot
     public void printByCampus() { } //ordered by campus and building/room
     public void printByDepartment(){ } //ordered by department
