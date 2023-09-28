64140d3abbb8bc5a557a1f00c193f5583b76df40
Aveesh22 <68564656+Aveesh22@users.noreply.github.com>
2023-09-28 02:08:54 -0400

    Add proper JavaDoc comments for generation and edit behavior of methods in the Event class

--- src/main/java/eventorganizer/Event.java
+++ src/main/java/eventorganizer/Event.java
@@ -12,7 +12,16 @@ public class Event implements Comparable<Event>
     private Contact contact; //include the department name and email
     private int duration; //in minutes
 
-    public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration) {
+    /** Event class constructor which instantiates the
+     * date, startTime, location, contact, and duration variables
+     * @param date the Date of the Event
+     * @param startTime the startTime of the Event
+     * @param location the location the Event takes place
+     * @param contact the contact person for the Event
+     * @param duration the duration of the Event
+     */
+    public Event(Date date, Timeslot startTime, Location location, Contact contact, int duration)
+    {
         this.date = date;
         this.startTime = startTime;
         this.location = location;
@@ -27,8 +36,10 @@ public class Event implements Comparable<Event>
      * @return boolean indicating if the 2 events equal
      */
     @Override
-    public boolean equals(Object obj) {
-        if (obj instanceof Event) {
+    public boolean equals(Object obj)
+    {
+        if (obj instanceof Event)
+        {
             Event event = (Event) obj;
             return event.date.equals(this.date) && //dates are equal
                     event.startTime.equals(this.startTime) && //starting times are equal
@@ -42,11 +53,13 @@ public class Event implements Comparable<Event>
      * @return the textual representation of an event
      */
     @Override
-    public String toString() {
-        Timeslot endTime = addMinutes(startTime, duration);
-        assert endTime != null;
+    public String toString()
+    {
+        //Timeslot endTime = addMinutes(startTime, duration);
+
+        //assert endTime != null;
         return "[eventorganizer.Event eventorganizer.Date:: " + date.toString() + "]" +
-                "[Start: " + startTime.toString() + "] " + "[End: " + endTime.toString() + "]" +
+                "[Start: " + startTime.toString() + "] " + "[End: " + //endTime.toString() + "]" +
                 "@" + location.name() + " " +
                 "(" + location.getBuilding() + ", " + location.getCampus() + ") " +
                 "[eventorganizer.Contact: " + contact.getDepartment().getFullName() +
@@ -55,15 +68,18 @@ public class Event implements Comparable<Event>
 
     /**
      * This method compares 2 events first by their dates, then by timeslots.
+     * @param event the event compared to by the current event
      * @return -1, 0, or 1
      */
     @Override
-    public int compareTo(Event event) {
+    public int compareTo(Event event)
+    {
         if ((this.date.compareTo(event.date)) > 0)
             return 1;
         else if ((this.date.compareTo(event.date)) < 0)
             return -1;
-        else {
+        else
+        {
             if ((this.startTime.compareTo(event.startTime)) > 0)
                 return 1;
             else if ((this.startTime.compareTo(event.startTime)) < 0)
@@ -73,27 +89,24 @@ public class Event implements Comparable<Event>
         }
     }
 
-
-
-
-
     /**
-     * This method adds minutes to the current timeslot time
-     * @param duration amount of minutes to add
-     * @return new Timeslot
-     */
-    private static Timeslot addMinutes(Timeslot startTime, int duration) {
-        return startTime;
-    }
-
-
-
-
-    /**
-     * Testbed main to exercise the equals() method.
+     * Testbed main to test methods in the Event class
      * @param args command line arguments.
      */
-    public static void main(String[] args) {
-        //run tests
+    public static void main(String[] args)
+    {
+        Date date = new Date("01/01/1999");
+        Date d = new Date("05/27/2000");
+        Timeslot startTime = Timeslot.MORNING;
+        Location location1 = Location.AB2225;
+        Location location2 = Location.TIL232;
+        Department department = Department.CS;
+        Contact contact = new Contact(department);
+        int duration1 = 80;
+        Event event1 = new Event(date, startTime, location1, contact, duration1);
+        Event event2 = new Event(date, startTime, location2, contact, duration1);
+        Event event3 = new Event(d, startTime, location1, contact, duration1);
+        System.out.println(event1.equals(event2));
+        System.out.println(event1.compareTo(event3));
     }
 }
